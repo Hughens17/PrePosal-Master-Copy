6faa864b1ba4fb32e186ffbd0d7503adedec1d15
Hughens17 <113854769+Hughens17@users.noreply.github.com>
2022-11-08 13:12:14 -0500

    Add files via upload

---
+++ pre posal master copy.py
@@ -0,0 +1,13 @@
+# Preproposal
+
+## What idea(s) do you have for your final project?
+
+I'm thinking of making a funny website which is going to link to funny internet videos or have programs that will allow the user to create mad libs.
+
+## If you plan to collaborate with one or two classmates, what are their names?
+
+No collaborators
+
+## Do you have any questions of your own?
+
+No
